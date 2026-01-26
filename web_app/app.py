280a65e385e44aced985c4761298ee08e12f5693
Gerhardt Schmidt <gerhardtschmidt2233@gmail.com>
2026-01-26 15:40:20 +0100

    implemented: solving through manual input

--- web_app/app.py
+++ web_app/app.py
@@ -1,4 +1,12 @@
-from flask import Flask, render_template
+import os
+import sys
+from flask import Flask, render_template, request
+
+parent_dir = os.path.dirname(os.path.dirname(__file__))
+sys.path.insert(1, parent_dir)
+import scripts.sudoku_solver as sudoku_solver
+
+SOLVER: sudoku_solver.Solver
 
 app = Flask(__name__)
 
@@ -12,10 +20,23 @@ def index():
 def manual_input():
     return render_template('manual_input.html')
 
-@app.route('/solution')
+
+@app.route('/solution', methods=['POST'])
 def solution():
-    solution_arr = [[i for i in range(1, 10)]] * 9  #
-    return render_template('solution.html', solution_arr=solution_arr)
+    cell_values = []
+    for i in range(81):
+        cell_values.append(request.form.get(f"cell_{i}", "0"))
+    quiz_str = "".join(cell_values)
+    initial_grid = sudoku_solver.quiz_str_to_grid(quiz_str)
+    SOLVER = sudoku_solver.Solver(initial_grid)
+    solution_gid = SOLVER.solve()
+    solved = SOLVER.is_sudoku_solved()
+    #  solution_arr = [[i for i in range(1, 10)]] * 9
+    return render_template(
+        'solution.html',
+        solution_grid=solution_gid,
+        solved=solved
+        )
 
 
 if __name__ == '__main__':
